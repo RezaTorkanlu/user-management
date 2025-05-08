01aa1daf7c5ca9586c82e63c81ff5492979eec86
RezaTorkanlu <reza.torkanlu.81@gmail.com>
2025-05-08 19:14:58 +0430

    create user page added

---
+++ src/app/(root)/users/create/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import UserForm from '@/components/UI/UserForm';
+import { useUserContext } from '@/context/UserContext';
+import { NewUser } from '@/types/users';
+import { useRouter } from 'next/navigation';
+import React from 'react'
+import { toast } from 'react-toastify';
+
+const CreateUserPage = () => {
+  const { addUser } = useUserContext()
+  const router = useRouter()
+
+  const handleSubmit = async (data: NewUser) => {
+    try {
+      await addUser(data)
+      toast.success("User created successfully")
+      router.push('/')
+    } catch (error) {
+      console.error("Error creating user:", error)
+      toast.error("Error creating user")
+    }
+  }
+  return (
+    <div className=''>
+      <h1 className='my-5 text-center font-bold text-xl'>Create New User</h1>
+      <UserForm onSubmit={handleSubmit} />
+    </div>
+  )
+}
+
+
+export default CreateUserPage;
