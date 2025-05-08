9f76a5622f29ebf151a7293d946f6be252ec4add
RezaTorkanlu <reza.torkanlu.81@gmail.com>
2025-05-08 19:13:56 +0430

    navbar updated

--- src/components/UI/Navbar.tsx
+++ src/components/UI/Navbar.tsx
@@ -1,57 +1,22 @@
-import React from 'react'
-import Image from 'next/image'
-import logo from '@/app/favicon.ico'
-import Link from 'next/link'
+import React from "react";
+import Image from "next/image";
+import logo from "@/app/favicon.ico";
+import Link from "next/link";
 
 const Navbar = () => {
-
-  const navLinks = [
-    {
-      id: 1,
-      link: "/",
-      title:'reza'
-    },
-    {
-      id: 2,
-      link: "/",
-      title:'reza'
-    },
-    {
-      id: 3,
-      link: "/",
-      title:'reza'
-    },
-    {
-      id: 4,
-      link: "/",
-      title:'reza'
-    },
-  ]
-  
   return (
-    <nav className='flex justify-between items-center shadow-2xl p-5 rounded-2xl'>
-      <div className='navFlex'>
-        <Image
-          src={logo}
-          width={32}
-          height={32}
-          alt='logo-icon'
-        />
-        <p className='text-xl mx-4'>torkan project</p>
+    <nav className="flex justify-between items-center shadow-2xl p-5 rounded-2xl min-sm: ">
+      <div className="flex items-center ">
+        <Image src={logo} width={32} height={32} alt="logo-icon" />
+        <p className="text-xl mx-4 max-sm:hidden">torkan project</p>
       </div>
-      <div className="">
-        <ul className='navFlex'>
-        {navLinks.map((link) => (
-          <li key={link.id} className='mx-5'>
-            <Link href={link.link}>
-              {link.title}
-            </Link>
-          </li>
-        ))}
-          </ul>
-      </div>
+      <div className="border  p-2 rounded-2xl flex items-center gap-4 bg-gray-800 text-white shadow-xl">
+          <Link href="/users/create" className="hover:text-gray-300">
+            Create User
+          </Link>
+        </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
