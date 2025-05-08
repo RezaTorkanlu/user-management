d3530c3846d45649ce1f163a45da410ca33826f4
RezaTorkanlu <reza.torkanlu.81@gmail.com>
2025-05-08 19:10:18 +0430

    types added

--- src/types/users.ts
+++ src/types/users.ts
@@ -2,5 +2,8 @@ export interface User{
   id: number,
   name: string,
   email: string;
-  phone?: number;
+  phone: number | string;
 }
+
+export type NewUser = Omit<User, 'id'>
+export type UpdateUser = Partial<Omit<User, 'id' >>
