cfabf490a5f65b4551dea9d163aa7e401b0eef7d
RezaTorkanlu <reza.torkanlu.81@gmail.com>
2025-05-08 19:14:42 +0430

    edit user page added

---
+++ src/app/(root)/users/edit/[id]/page.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import UserForm from "@/components/UI/UserForm"
+import { useUserContext } from "@/context/UserContext"
+import { UpdateUser } from "@/types/users"
+import { useParams, useRouter } from "next/navigation"
+import { toast } from "react-toastify"
+
+const EditUserPage = () => {
+  const { users, editUser } = useUserContext()
+  const { id } = useParams()
+  const router = useRouter()
+
+  const userToEdit = users.find(user=>user.id === Number(id))
+
+  const handleSubmit = async (data: UpdateUser) => {
+    try {
+      await editUser(Number(id), data)
+      toast.success("User updated successfully")
+      router.push('/')
+    } catch (error) {
+      console.error("Error updating user:", error)
+      toast.error("Error updating user")
+    }
+  }
+  if (!userToEdit) return <div className="text-center text-2xl font-bold my-10">User not found!</div>
+  
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-xl font-bold text-center my-5">Edit for {userToEdit?.name}</h1>
+      <UserForm
+        initialData={userToEdit}
+        onSubmit={handleSubmit}
+      />
+    </div>
+  )
+}
+
+export default EditUserPage
