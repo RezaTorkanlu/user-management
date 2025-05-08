985dc4313661d05553b03f1ed070435de95f4a87
RezaTorkanlu <reza.torkanlu.81@gmail.com>
2025-05-08 19:15:57 +0430

    loading page and error page added

---
+++ src/app/Error.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div className='text-red-800 h-98 flex items-center justify-center '>Error please call suports</div>
+  )
+}
+
+export default Error
---
+++ src/app/Loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='w-full h-98 flex justify-center items-center text-2xl font-semibold'>Loading ...</div>
+  )
+}
+
+export default Loading
