77fdb1b0728716e75de7b7ab8b69ae6f1182fa88
RezaTorkanlu <reza.torkanlu.81@gmail.com>
2025-05-08 19:15:20 +0430

    home page updated

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,17 @@
-export default function Home() {
+import UserListContainer from "@/container/UserListContainer";
+import { User } from "@/types/users";
+
+type Props = {
+  initialUsers : User[]
+}
+
+export default function Home({ initialUsers }: Props) {
   return (
     <>
-      <h2 className="flex justify-center text-2xl font-semibold mt-5">
+      <h2 className="flex justify-center text-2xl font-semibold mt-5 max-sm:text-xl max-sm:text-center ">
         Hello And Wellcome To My Project
       </h2>
-      
+      <UserListContainer initialUsers={initialUsers} />
     </>
   );
 }
