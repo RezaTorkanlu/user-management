daec088621186cb8c2ff48f84c91e848054b1474
RezaTorkanlu <reza.torkanlu.81@gmail.com>
2025-05-16 13:02:43 +0430

    change import address

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/UI/Navbar";
+import Navbar from "@/components/Navbar";
 import { ToastContainer } from "react-toastify";
 import { UserProvider } from "@/context/UserContext";
 
