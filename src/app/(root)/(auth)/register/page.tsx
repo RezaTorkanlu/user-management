99e5f021afe29e912339c88b61637a0c9b17d4a6
RezaTorkanlu <reza.torkanlu.81@gmail.com>
2025-05-16 12:59:53 +0430

    create register page

---
+++ src/app/(root)/(auth)/register/page.tsx
@@ -0,0 +1,13 @@
+import RegisterFrom from "@/components/RegisterFrom"
+
+const RegistePage = () => {
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-center my-10 font-bold text-xl">Register</h1>
+      <RegisterFrom />
+    </div>
+  )
+}
+
+export default RegistePage
