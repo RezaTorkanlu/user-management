6d3759cf23e06f3e093155088f7297fc61531e0b
RezaTorkanlu <reza.torkanlu.81@gmail.com>
2025-05-31 20:05:23 +0430

    change new path 2

---
+++ src/app/(root)/auth/register/page.tsx
@@ -0,0 +1,21 @@
+'use client'
+import RegisterFrom from "@/components/RegisterFrom"
+import { RegisterUser } from "@/types/users"
+import { useRouter } from "next/navigation"
+
+const RegistePage = () => {
+  const router = useRouter()
+
+  const handleSubmit = async (data: RegisterUser): Promise<void> => {
+    console.log(data)
+    router.push('/')
+  }
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-center my-10 font-bold text-xl">Register</h1>
+      <RegisterFrom onSubmit={handleSubmit} />
+    </div>
+  )
+}
+
+export default RegistePage
